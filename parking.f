b62f3d405c3bddd3c981aa1c05de628a3b6d2af9
Nilson Perboni Neto <nilsonnetonapn@gmail.com>
2015-12-18 17:02:12 -0200

    Added 2 cases

--- parking.f
+++ parking.f
@@ -6,16 +6,30 @@ C
       real*8 tec(2000),ts(2000),tr(2000),tini,tfim(2000),wq,tsis,lq,ls
       real*8 stls,stec,sts,swq,nwait,stsis,r1,r2,r3,r4,r5,r6
       integer iseed,num,gerar,eCheg,sCheg,eSaid,sSaid,m,is
-C      open(1, file= 'sim1.a.csv',status = 'unknown')
-C      open(2, file= 'sim1.b.csv',status = 'unknown')
-      open(1, file= 'r1.dat',status = 'unknown')
-      open(2, file= 'r2.dat',status = 'unknown')
-      open(3, file= 'r3.dat',status = 'unknown')
-      open(4, file= 'r4.dat',status = 'unknown')
-      open(5, file= 'r5.dat',status = 'unknown')
-      open(6, file= 'r6.dat',status = 'unknown')
-      open(7, file= 'res.dat',status = 'unknown')
 
+C      open(2, file= 'sim1.b.csv',status = 'unknown')
+      open(1, file= 'r11.dat',status = 'unknown')
+      open(2, file= 'r12.dat',status = 'unknown')
+      open(3, file= 'r13.dat',status = 'unknown')
+      open(4, file= 'r14.dat',status = 'unknown')
+      open(5, file= 'r15.dat',status = 'unknown')
+      open(6, file= 'r16.dat',status = 'unknown')
+      open(7, file= 'res.csv',status = 'unknown')
+      open(21, file= 'r21.dat',status = 'unknown')
+      open(22, file= 'r22.dat',status = 'unknown')
+      open(23, file= 'r23.dat',status = 'unknown')
+      open(24, file= 'r24.dat',status = 'unknown')
+      open(25, file= 'r25.dat',status = 'unknown')
+      open(26, file= 'r26.dat',status = 'unknown')
+      open(27, file= 'res2.csv',status = 'unknown')
+      open(31, file= 'r31.dat',status = 'unknown')
+      open(32, file= 'r32.dat',status = 'unknown')
+      open(33, file= 'r33.dat',status = 'unknown')
+      open(34, file= 'r34.dat',status = 'unknown')
+      open(35, file= 'r35.dat',status = 'unknown')
+      open(36, file= 'r36.dat',status = 'unknown')
+      open(37, file= 'res3.csv',status = 'unknown')
+C      open(8, file= 'sim1.csv',status = 'unknown')
 C
 C     Formatos
 C
@@ -36,8 +50,8 @@ C12   format(F10.5)
      *hegada (Ls),Pessoas na Fila na Saida (PS),Tempo de Sistema Livre (
      *TLS)')
 16    format(F15.10,',',F15.10,',',F15.10,',',F15.10)
-17    format(F15.9,',',F15.9,',',F15.9,',',F15.9,',',F15.9,',',F15.9,','
-     *,I5)
+17    format(I5,',',F15.9,',',F15.9,',',F15.9,',',F15.9,',',F15.9,',',F1
+     *5.9,',',F15.9)
 18    format(I5,',',F15.10)
 C15   format('Fim')
 C16   format(I5,F15.10)
@@ -48,7 +62,7 @@ C
 
       is = 1
       num = 1000
-      gerar = 100
+      gerar = 500
 
 C
 C     Inicializacao     
@@ -67,6 +81,12 @@ C
       swq = 0
       nwait = 0
       stsis = 0
+      r1 = 0
+      r2 = 0
+      r3 = 0
+      r4 = 0
+      r5 = 0
+      r6 = 0
       
       do i=1,gerar
           tec(i) = 0
@@ -90,8 +110,6 @@ C
         pmod = 2147483647.D0
         dmax = 1.0D0/pmod
         num = num + 1
-        a = 10
-        b = 20
 
 C       mudando o iseed
 
@@ -99,10 +117,15 @@ C       mudando o iseed
         do i=2,num
            rnum(i) = cong16807(iseed)
         end do
-        
+
+C       Calculo com intervalo normal
+
+        a = 10
+        b = 20
+
         alfa = 4.0D0/60.0D0
         med = 1/alfa
-        
+
         do i=1,gerar
             tec(i)=-log(rnum(i+2))/alfa
             ts(i)=(b-a)*rnum(i+2)+a
@@ -130,7 +153,7 @@ C       mudando o iseed
                     tls = 0
                 end if
             end if
-            
+
             tsis = tfim(i) - tr(i)
             stls = stls + tls
             stec = stec + tec(i)
@@ -140,7 +163,7 @@ C       mudando o iseed
                nwait = nwait + 1.0D0
             end if
             stsis = stsis + tsis
-C     write(1,13)is,tec(i),ts(i),tr(i),tini,tfim(i),wq,tsis,0,0,0,tls
+C        write(8,13)is,tec(i),ts(i),tr(i),tini,tfim(i),wq,tsis,0,0,0,tls
         end do
 
         do k=1,gerar
@@ -168,21 +191,21 @@ C     write(1,13)is,tec(i),ts(i),tr(i),tini,tfim(i),wq,tsis,0,0,0,tls
 
 C          write(2,16) lq,ls,ps
         end do
-        
+
         r1 = stls/tfim(gerar)
         r2 = stec/gerar
         r3 = sts/gerar
         r4 = swq/gerar
         r5 = nwait/gerar
         r6 = stsis/gerar
-        
+
         write(1,18)is,r1
         write(2,18)is,r2
         write(3,18)is,r3
         write(4,18)is,r4
         write(5,18)is,r5
         write(6,18)is,r6
-        write(7,17)r1,r2,r3,r4,r5,r6,is
+        write(7,17)is,r1,r2,r3,r4,r5,r6,tfim(gerar)
 
         tini = 0
         wq = 0
@@ -197,6 +220,244 @@ C          write(2,16) lq,ls,ps
         swq = 0
         nwait = 0
         stsis = 0
+        r1 = 0
+        r2 = 0
+        r3 = 0
+        r4 = 0
+        r5 = 0
+        r6 = 0
+
+        do m=1,gerar
+            tec(m) = 0
+            ts(m) = 0
+            tr(m) = 0
+            tfim(m) = 0
+        end do
+
+C       Calculo com intervalo dobrado
+
+        a = 20
+        b = 40
+
+        alfa = 4.0D0/60.0D0
+        med = 1/alfa
+
+        do i=1,gerar
+            tec(i)=-log(rnum(i+2))/alfa
+            ts(i)=(b-a)*rnum(i+2)+a
+            if (i == 1) then
+                tr(i) = tec(i)
+                tini = tec(i)
+                tfim(i) = ts(i) + tini
+                wq = 0
+                lq = 0
+                ls = 1
+                tls = tec(i)
+            else
+                tr(i) = tr(i-1) + tec(i)
+                if (tr(i) <= tfim(i-1)) then
+                    tini = tfim(i-1)
+                    wq = tfim(i-1) - tr(i)
+                else
+                    tini = tr(i)
+                    wq = 0
+                end if
+                tfim(i) = ts(i) + tini
+                if (tr(i) >= tfim(i-1)) then
+                    tls = tr(i) - tfim(i-1)
+                else
+                    tls = 0
+                end if
+            end if
+
+            tsis = tfim(i) - tr(i)
+            stls = stls + tls
+            stec = stec + tec(i)
+            sts = sts + ts(i)
+            swq = swq + wq
+            if (wq == 0.0D0) then
+               nwait = nwait + 1.0D0
+            end if
+            stsis = stsis + tsis
+C        write(8,13)is,tec(i),ts(i),tr(i),tini,tfim(i),wq,tsis,0,0,0,tls
+        end do
+
+        do k=1,gerar
+           eCheg = 0
+           sCheg = 0
+           eSaid = 0
+           sSaid = 0
+           do j=1,gerar
+           if (tr(j) <= tr(k)) then
+              eCheg = eCheg + 1.0D0
+           end if
+           if (tfim(j) <= tr(k)) then
+              sCheg = sCheg + 1.0D0
+           end if
+           if (tr(j) <= tfim(k)) then
+              eSaid = eSaid + 1.0D0
+           end if
+           if (tfim(j) <= tfim(k)) then
+              sSaid = sSaid + 1.0D0
+           end if
+           end do
+           lq = eCheg - sCheg - 1.0D0
+           ls = lq + 1.0D0
+           ps = eSaid - sSaid
+
+C          write(2,16) lq,ls,ps
+        end do
+
+        r1 = stls/tfim(gerar)
+        r2 = stec/gerar
+        r3 = sts/gerar
+        r4 = swq/gerar
+        r5 = nwait/gerar
+        r6 = stsis/gerar
+
+        write(21,18)is,r1
+        write(22,18)is,r2
+        write(23,18)is,r3
+        write(24,18)is,r4
+        write(25,18)is,r5
+        write(26,18)is,r6
+        write(27,17)is,r1,r2,r3,r4,r5,r6,tfim(gerar)
+
+        tini = 0
+        wq = 0
+        tsis = 0
+        lq = 0
+        ls = 0
+        ps = 0
+        tls = 0
+        stls = 0
+        stec = 0
+        sts = 0
+        swq = 0
+        nwait = 0
+        stsis = 0
+        r1 = 0
+        r2 = 0
+        r3 = 0
+        r4 = 0
+        r5 = 0
+        r6 = 0
+
+        do m=1,gerar
+            tec(m) = 0
+            ts(m) = 0
+            tr(m) = 0
+            tfim(m) = 0
+        end do
+
+C       Calculo com intervalo reduzido na metade
+
+        a = 5
+        b = 10
+
+        alfa = 4.0D0/60.0D0
+        med = 1/alfa
+
+        do i=1,gerar
+            tec(i)=-log(rnum(i+2))/alfa
+            ts(i)=(b-a)*rnum(i+2)+a
+            if (i == 1) then
+                tr(i) = tec(i)
+                tini = tec(i)
+                tfim(i) = ts(i) + tini
+                wq = 0
+                lq = 0
+                ls = 1
+                tls = tec(i)
+            else
+                tr(i) = tr(i-1) + tec(i)
+                if (tr(i) <= tfim(i-1)) then
+                    tini = tfim(i-1)
+                    wq = tfim(i-1) - tr(i)
+                else
+                    tini = tr(i)
+                    wq = 0
+                end if
+                tfim(i) = ts(i) + tini
+                if (tr(i) >= tfim(i-1)) then
+                    tls = tr(i) - tfim(i-1)
+                else
+                    tls = 0
+                end if
+            end if
+
+            tsis = tfim(i) - tr(i)
+            stls = stls + tls
+            stec = stec + tec(i)
+            sts = sts + ts(i)
+            swq = swq + wq
+            if (wq == 0.0D0) then
+               nwait = nwait + 1.0D0
+            end if
+            stsis = stsis + tsis
+C        write(8,13)is,tec(i),ts(i),tr(i),tini,tfim(i),wq,tsis,0,0,0,tls
+        end do
+
+        do k=1,gerar
+           eCheg = 0
+           sCheg = 0
+           eSaid = 0
+           sSaid = 0
+           do j=1,gerar
+           if (tr(j) <= tr(k)) then
+              eCheg = eCheg + 1.0D0
+           end if
+           if (tfim(j) <= tr(k)) then
+              sCheg = sCheg + 1.0D0
+           end if
+           if (tr(j) <= tfim(k)) then
+              eSaid = eSaid + 1.0D0
+           end if
+           if (tfim(j) <= tfim(k)) then
+              sSaid = sSaid + 1.0D0
+           end if
+           end do
+           lq = eCheg - sCheg - 1.0D0
+           ls = lq + 1.0D0
+           ps = eSaid - sSaid
+
+C          write(2,16) lq,ls,ps
+        end do
+
+        r1 = stls/tfim(gerar)
+        r2 = stec/gerar
+        r3 = sts/gerar
+        r4 = swq/gerar
+        r5 = nwait/gerar
+        r6 = stsis/gerar
+
+        write(31,18)is,r1
+        write(32,18)is,r2
+        write(33,18)is,r3
+        write(34,18)is,r4
+        write(35,18)is,r5
+        write(36,18)is,r6
+        write(37,17)is,r1,r2,r3,r4,r5,r6,tfim(gerar)
+
+        tini = 0
+        wq = 0
+        tsis = 0
+        lq = 0
+        ls = 0
+        ps = 0
+        tls = 0
+        stls = 0
+        stec = 0
+        sts = 0
+        swq = 0
+        nwait = 0
+        stsis = 0
+        r1 = 0
+        r2 = 0
+        r3 = 0
+        r4 = 0
+        r5 = 0
+        r6 = 0
 
         do m=1,gerar
             tec(m) = 0
